ae408d0569bc6f3526ca9174c14f6a548a65417a
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-09-05 18:46:40 -0300

    first component

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return <h1>HELLO WORLD!!</h1>;
-}
+import { App } from "./HelloWorldApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
