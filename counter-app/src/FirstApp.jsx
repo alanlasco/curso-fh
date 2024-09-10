6460b1b1ee16ffad9d9cbe0af5f9aac84ba8ae11
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-09-10 16:18:19 -0300

    communication between components

--- counter-app/src/FirstApp.jsx
+++ counter-app/src/FirstApp.jsx
@@ -2,17 +2,17 @@ const say = () => {
   return "Hola ALan";
 };
 
-export const FirstApp = () => {
+export const FirstApp = ({ name, surname }) => {
   //   const newMessage = {
   //     message: "alan",
   //     title: "lasco",
   //   };
-
+  console.log(name);
   return (
     <>
       {/*fragmento */}
       {/* <code>{JSON.stringify(newMessage)}</code> */}
-      <h1>Hola mundo</h1>
+      <h1>{name + " " + surname}</h1>
       <h2>{say()}</h2>
     </>
   );
--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -7,6 +7,6 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <FirstApp />
+    <FirstApp name="Hola soy Alan" surname="lasco" />
   </React.StrictMode>
 );
