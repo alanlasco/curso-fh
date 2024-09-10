f5b87cf2a5a1cc4def60921dd37b74b40aee4f3e
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-09-10 17:40:42 -0300

    counter <pp

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import { App } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
 import "./index.css";
+import { CounterApp } from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <FirstApp name="alan" surname="lasco" />
+    {/* <FirstApp name="alan" surname="lasco" /> */}
+    <CounterApp value={1} />
   </React.StrictMode>
 );
