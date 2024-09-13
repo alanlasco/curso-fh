fa6f1d0b820e23f21416cde6cbca2593ed6d4431
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-09-13 20:29:04 -0300

    toEqual toStrictEqual

---
+++ counter-app/tests/07-deses-arr.test.js
@@ -0,0 +1,16 @@
+import { number } from "prop-types";
+import { retornaArreglo } from "../src/base-pruebas/07-deses-arr";
+
+describe("peubas en 07-deses-arr", () => {
+  test("debe retornar un string y un numero", () => {
+    const [letters, numbers] = retornaArreglo();
+
+    expect(letters).toBe("ABC");
+    expect(numbers).toBe(123);
+
+    expect(typeof letters).toBe("string");
+    expect(typeof numbers).toBe("number");
+
+    expect(letters).toEqual(expect.any(String)); // en caso de que queira recibir cualquier tipo de string en  vez de abc
+  });
+});
