a96e2de9e3a92685158e8af4dfa1cf90d54b6016
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-12-02 19:24:44 -0300

    useffect

--- gift-expert-app/src/components/GifGrid.jsx
+++ gift-expert-app/src/components/GifGrid.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react"; // dispara efectos secundarios, cuando algo cambie quiero disparar un efecto
+import { getGifs } from "../helpers/getGifs";
+
 export const GifGrid = ({ category }) => {
+  useEffect(() => {
+    getGifs(category); //se sigue disparando dos veces porque react lo hace en el modo estricto
+  }, []);
+
   return (
     <>
       <h3>{category}</h3>
