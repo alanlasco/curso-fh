8a2bf4b096b4c6e2f05e825a6cf205defd1e4513
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-12-03 18:54:22 -0300

    fin del proyecto, falta el test

--- gift-expert-app/src/components/GifGrid.jsx
+++ gift-expert-app/src/components/GifGrid.jsx
@@ -1,14 +1,28 @@
-import { useEffect } from "react"; // dispara efectos secundarios, cuando algo cambie quiero disparar un efecto
+import { useEffect, useState } from "react"; // dispara efectos secundarios, cuando algo cambie quiero disparar un efecto
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  useEffect(() => {
-    getGifs(category); //se sigue disparando dos veces porque react lo hace en el modo estricto
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
+      {
+        //isLoading && (<h2>cargando....</h2>)
+        isLoading ? <h2>cargando....</h2> : null
+      }
+
+      <div className="card-grid">
+        {images.map(
+          (
+            image // (i)=>{ i.id i.title}
+          ) => (
+            <GifItem key={image.id} {...image} /> //para que el gif item reciba cada una de las propiedades como props
+          )
+        )}
+      </div>
     </>
   );
 };
---
+++ gift-expert-app/src/hooks/useFetchGifs.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getImages(category); //se sigue disparando dos veces porque react lo hace en el modo estricto
+  }, []);
+
+  return {
+    images: images,
+    isLoading: isLoading,
+  };
+};
