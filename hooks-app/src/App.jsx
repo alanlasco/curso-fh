59104527f91ff5ff0bf11c398f57e486b1dcb6f8
alanlasco <61526254+alanlasco@users.noreply.github.com>
2025-03-11 19:39:11 -0300

    hooks-1

---
+++ hooks-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { CounterApp } from "./use-State/CounterApp";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HooksApp</h1>
+      <CounterApp />
+    </div>
+  );
+}
+
+export default App;
